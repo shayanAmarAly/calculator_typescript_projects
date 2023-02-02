e84cf8188358550293602a6d0342096b194221db
shayanAmarAly <101214599+shayanAmarAly@users.noreply.github.com>
2023-02-02 01:36:43 -0800

    calculator typescript project "first commit"

---
+++ calculator.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+const response = await inquirer.prompt([
+    {
+        name: "operand1",
+        type: "number",
+        message: "Kindly enter your first operand"
+    },
+    {
+        name: "operand2",
+        type: "number",
+        message: "Kindly enter your second operand"
+    },
+    {
+        name: "operation",
+        type: "list",
+        choices: ["+", "-", "/", "*"],
+        message: "Select operation"
+    },
+]);
+
+// Create structure to handle (response)
+const {operand1, operand2, operation} = response;
+
+if (operand1 && operand2 && operation) {
+    let result: number = 0;
+    if (operation == "+") {
+        result = operand1 + operand2;
+    } 
+    else if(operation == "-"){
+        result = operand1 - operand2;
+    }
+    else if(operation == "/"){
+        result = operand1 / operand2;
+    }
+    else if(operation == "*"){
+        result = operand1 * operand2;
+    }
+    console.log(`Your Result is ${result}` )
+} else {
+    console.log("Kindle enter operands and give operations carefully")
+}
+
+
